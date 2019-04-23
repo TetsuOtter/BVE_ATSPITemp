09ec298c147e049c77c5ad814f5f005b9054e1da
ＴＲ <public@technotter.com>
2019-04-23 13:04:01 +0900

    引数追加

--- PITempCS/PILoad.cs
+++ PITempCS/PILoad.cs
@@ -10,37 +10,37 @@ namespace PITempCS
   {
   
     /*外部PIを読み込む際はコメントアウト解除をする。
-    public const CallingConvention CalCnv = CallingConvention.StdCall;
+    private const CallingConvention CalCnv = CallingConvention.StdCall;
     [DllImport("ATS.dll", CallingConvention = CalCnv)]
     internal static extern void Load();
     [DllImport("ATS.dll", CallingConvention = CalCnv)]
     internal static extern void Dispose();
     [DllImport("ATS.dll", CallingConvention = CalCnv)]
-    internal static extern void SetVehicleSpec();
+    internal static extern void SetVehicleSpec(Spec s);
     [DllImport("ATS.dll", CallingConvention = CalCnv)]
-    internal static extern void Initialize();
+    internal static extern void Initialize(int s);
     [DllImport("ATS.dll", CallingConvention = CalCnv)]
-    internal static extern void Elapse();
+    internal static extern unsafe void Elapse(State s, int* Pa, int* So);
     [DllImport("ATS.dll", CallingConvention = CalCnv)]
-    internal static extern void SetPower();
+    internal static extern void SetPower(int p);
     [DllImport("ATS.dll", CallingConvention = CalCnv)]
-    internal static extern void SetBrake();
+    internal static extern void SetBrake(int b);
     [DllImport("ATS.dll", CallingConvention = CalCnv)]
-    internal static extern void SetReverser();
+    internal static extern void SetReverser(int r);
     [DllImport("ATS.dll", CallingConvention = CalCnv)]
-    internal static extern void KeyDown();
+    internal static extern void KeyDown(int k);
     [DllImport("ATS.dll", CallingConvention = CalCnv)]
-    internal static extern void KeyUp();
+    internal static extern void KeyUp(int k);
     [DllImport("ATS.dll", CallingConvention = CalCnv)]
-    internal static extern void HornBlow();
+    internal static extern void HornBlow(int k);
     [DllImport("ATS.dll", CallingConvention = CalCnv)]
     internal static extern void DoorOpen();
     [DllImport("ATS.dll", CallingConvention = CalCnv)]
     internal static extern void DoorClose();
     [DllImport("ATS.dll", CallingConvention = CalCnv)]
-    internal static extern void SetSignal();
+    internal static extern void SetSignal(int s);
     [DllImport("ATS.dll", CallingConvention = CalCnv)]
-    internal static extern void SetBeaconData();
+    internal static extern void SetBeaconData(Beacon b);
     //[DllImport("ATS.dll", CallingConvention = CalCnv)]
     //internal static extern void GetPluginVersion();
     */
