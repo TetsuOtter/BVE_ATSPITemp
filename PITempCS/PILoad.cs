7e21ebe24d7e5442fcf2a0223157104bbaf1151f
ＴＲ <public@technotter.com>
2019-04-23 11:30:25 +0900

    Add PI Loading Support

---
+++ PITempCS/PILoad.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+namespace PITempCS
+{
+  static internal class PILoad
+  {
+  
+    /*外部PIを読み込む際はコメントアウト解除をする。
+    public const CallingConvention CalCnv = CallingConvention.StdCall;
+    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    internal static extern void Load();
+    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    internal static extern void Dispose();
+    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    internal static extern void SetVehicleSpec();
+    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    internal static extern void Initialize();
+    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    internal static extern void Elapse();
+    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    internal static extern void SetPower();
+    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    internal static extern void SetBrake();
+    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    internal static extern void SetReverser();
+    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    internal static extern void KeyDown();
+    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    internal static extern void KeyUp();
+    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    internal static extern void HornBlow();
+    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    internal static extern void DoorOpen();
+    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    internal static extern void DoorClose();
+    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    internal static extern void SetSignal();
+    [DllImport("ATS.dll", CallingConvention = CalCnv)]
+    internal static extern void SetBeaconData();
+    //[DllImport("ATS.dll", CallingConvention = CalCnv)]
+    //internal static extern void GetPluginVersion();
+    */
+  }
+}
